21ab7964530cf10bb5a53ba121750814633f229f
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2023-08-27 23:02:33 +0300

    Created tableView and Model

---
+++ ContactsV1/Model/Contact.swift
@@ -0,0 +1,58 @@
+//
+//  Contact.swift
+//  ContactsV1
+//
+//  Created by Валерия Дементьева on 27.08.2023.
+//
+
+import Foundation
+import UIKit
+
+protocol ContactProtocol {
+    // Имя
+    var name: String { get set }
+    // Номер телефона
+    var phone: String { get set }
+    // Изображение
+    var image: String { get set }
+    
+}
+
+struct Contact: ContactProtocol {
+    var name: String
+    var phone: String
+    var image: String
+
+}
+
+extension Contact {
+    static let info = [
+        Contact(name: "Vasya 1", phone: "+7 987 987 87 76", image: "person"),
+        Contact(name: "Vasya 2", phone: "+7 987 987 87 76", image: "person.fill"),
+        Contact(name: "Vasya 3", phone: "+7 987 987 87 76", image: "person.circle"),
+        Contact(name: "Vasya 4", phone: "+7 987 987 87 76", image: "person.fill.questionmark"),
+        Contact(name: "Vasya 5", phone: "+7 987 987 87 76", image: "person.fill"),
+        Contact(name: "Vasya 6", phone: "+7 987 987 87 76", image: "person"),
+        Contact(name: "Vasya 7", phone: "+7 987 987 87 76", image: "person.circle"),
+        Contact(name: "Vasya 8", phone: "+7 987 987 87 76", image: "person"),
+        Contact(name: "Vasya 9", phone: "+7 987 987 87 76", image: "person.fill"),
+        Contact(name: "Vasya 10", phone: "+7 987 987 87 76", image: "person.fill.questionmark"),
+        Contact(name: "Vasya 11", phone: "+7 987 987 87 76", image: "person.circle.fill"),
+        Contact(name: "Vasya 12", phone: "+7 987 987 87 76", image: "person.circle.fill"),
+        Contact(name: "Vasya 13", phone: "+7 987 987 87 76", image: "person.circle"),
+        Contact(name: "Vasya 14", phone: "+7 987 987 87 76", image: "person.fill.xmark"),
+        Contact(name: "Vasya 15", phone: "+7 987 987 87 76", image: "person"),
+        Contact(name: "Vasya 16", phone: "+7 987 987 87 76", image: "person.circle.fill"),
+        Contact(name: "Vasya 17", phone: "+7 987 987 87 76", image: "person.circle"),
+        Contact(name: "Vasya 18", phone: "+7 987 987 87 76", image: "person.fill.xmark"),
+        Contact(name: "Vasya 19", phone: "+7 987 987 87 76", image: "person.fill"),
+        Contact(name: "Vasya 20", phone: "+7 987 987 87 76", image: "person.fill.questionmark"),
+        Contact(name: "Vasya 21", phone: "+7 987 987 87 76", image: "person.circle.fill"),
+        Contact(name: "Vasya 22", phone: "+7 987 987 87 76", image: "person.circle.fill"),
+        Contact(name: "Vasya 23", phone: "+7 987 987 87 76", image: "person.circle"),
+        Contact(name: "Vasya 24", phone: "+7 987 987 87 76", image: "person.fill.xmark"),
+        Contact(name: "Vasya 25", phone: "+7 987 987 87 76", image: "person"),
+        Contact(name: "Vasya 26", phone: "+7 987 987 87 76", image: "person.circle.fill"),
+        Contact(name: "Vasya 27", phone: "+7 987 987 87 76", image: "person.circle")
+    ]
+}
--- ContactsV1/ViewController.swift
+++ ContactsV1/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+//свяжем сцену и созданную модель Contact. Свойство contacts – это массив контактов, элементы которого будут выведены в табличном представлении. При загрузке сцены данное свойство будет наполняться данными, а впоследствии использоваться для наполнения ячеек таблицы данными.
+    private var contacts = Contact.info
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //        версия 1
+        var cell: UITableViewCell
+
+        if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "MyCell") {
+            print("Используем старую ячейку для строки с индексом \(indexPath.row)")
+            cell = reuseCell
+        } else {
+            print("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+            cell = UITableViewCell(style: .value1, reuseIdentifier: "MyCell")
+        }
+        configure(cell: cell, for: indexPath)
+        return cell
+    }
+
+        private func configure(cell: UITableViewCell, for indexPath: IndexPath) {
+            var configuration = cell.defaultContentConfiguration()
+            configuration.text = contacts[indexPath.row].name
+            configuration.secondaryText = contacts[indexPath.row].phone
+            configuration.image = UIImage(systemName: contacts[indexPath.row].image)
+            cell.contentConfiguration = configuration
+        }
+    }
+//    версия 2
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        // производим попытку загрузки переиспользуемой ячейки
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell")
+//        else {
+//            print("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+//            let newCell = UITableViewCell(style: .value1, reuseIdentifier: "MyCell")
+//            configure(cell: newCell, for: indexPath)
+//            return newCell
+//        }
+//        print("Используем старую ячейку для строки с индексом \(indexPath.row)")
+//        configure(cell: cell, for: indexPath)
+//        return cell
+//    }
+
+    //версия 3
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//         // производим попытку загрузки переиспользуемой ячейки
+//         if let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell") {
+//             print("Используем старую ячейку для строки с индексом \(indexPath.row)")
+//             configure(cell: cell, for: indexPath)
+//             return cell
+//         } else {
+//             print("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+//             let newCell = UITableViewCell(style: .value1, reuseIdentifier: "MyCell")
+//             configure(cell: newCell, for: indexPath)
+//             return newCell
+//         }
+//     }
+//
 
